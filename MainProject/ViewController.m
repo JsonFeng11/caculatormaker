32a9a03a44625878f86e5268090c05f595cd3eca
JosnFeng <json_feng@outlook.com>
2017-12-13 21:39:23 +0800

    初始化

---
+++ MainProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MainProject
+//
+//  Created by fdd on 2017/12/11.
+//  Copyright © 2017年 fdd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
